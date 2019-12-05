69608e6571923c48211925598efdf571b2cce782
André Hatlo <ahatloj@gmail.com>
2019-12-05 11:23:43 +0100

    Updates

---
+++ precache-manifest.48768f98a37c6468a9495d23ff61718d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b663794d058d3fe26dbd066c3cbdab",
+    "url": "/react-dev-course/index.html"
+  },
+  {
+    "revision": "3b986d42b257fda53fa1",
+    "url": "/react-dev-course/static/css/main.c39e603c.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/react-dev-course/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "3b986d42b257fda53fa1",
+    "url": "/react-dev-course/static/js/main.f07ebeca.chunk.js"
+  },
+  {
+    "revision": "e45843b6e8d187c20eb7",
+    "url": "/react-dev-course/static/js/runtime-main.1ca05184.js"
+  }
+]);
